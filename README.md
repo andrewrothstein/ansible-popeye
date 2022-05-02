154ec613e1b33ef8acd16528991b52970d3b8f31
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-05-01 20:35:03 -0400

    popeye:0.10.0

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.popeye
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-popeye/actions/workflows/build.yml/badge.svg)
+
+Installs [popeye](https://github.com/derailed/popeye).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.popeye
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/derailed/popeye/releases/download
+APP=popeye
+
+dl() {
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
+    local platform="${os}_${arch}"
+    local file="${APP}_${platform}.${archive_type}"
+    local url=$MIRROR/v$ver/$file
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    # https://github.com/derailed/popeye/releases/download/v0.10.0/checksums.txt
+    local url="${MIRROR}/v${ver}/checksums.txt"
+    local lchecksums="$DIR/${APP}_${ver}_checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        curl -sSLf -o $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $lchecksums Darwin arm64
+    dl $ver $lchecksums Darwin x86_64
+    dl $ver $lchecksums Linux arm
+    dl $ver $lchecksums Linux arm64
+    dl $ver $lchecksums Linux x86_64
+    dl $ver $lchecksums Windows arm
+    dl $ver $lchecksums Windows arm64
+    dl $ver $lchecksums Windows x86_64
+}
+
+dl_ver 0.9.7
+dl_ver 0.9.8
+dl_ver ${1:-0.10.0}
