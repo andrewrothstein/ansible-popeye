52804b977c28288328bd28ca823af950e7132fe3
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-03-07 03:35:58 +0000

    popeye:0.22.1

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -14,14 +14,14 @@ dl() {
     local file="${APP}_${platform}.${archive_type}"
     local url=$MIRROR/v$ver/$file
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(grep -e "$file\$" $lchecksums | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
-    # https://github.com/derailed/popeye/releases/download/v0.10.0/checksums.txt
-    local url="${MIRROR}/v${ver}/checksums.txt"
-    local lchecksums="$DIR/${APP}_${ver}_checksums.txt"
+    # https://github.com/derailed/popeye/releases/download/v0.10.0/checksums.sha256
+    local url="${MIRROR}/v${ver}/checksums.sha256"
+    local lchecksums="$DIR/${APP}_${ver}_checksums.sha256"
     if [ ! -e $lchecksums ];
     then
         curl -sSLf -o $lchecksums $url
@@ -30,14 +30,12 @@ dl_ver() {
     printf "  # %s\n" $url
     printf "  '%s':\n" $ver
 
-    dl $ver $lchecksums Darwin arm64
-    dl $ver $lchecksums Darwin x86_64
-    dl $ver $lchecksums Linux arm
-    dl $ver $lchecksums Linux arm64
-    dl $ver $lchecksums Linux x86_64
-    dl $ver $lchecksums Windows arm
-    dl $ver $lchecksums Windows arm64
-    dl $ver $lchecksums Windows x86_64
+    dl $ver $lchecksums darwin arm64
+    dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums linux arm64
+    dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums windows arm64
+    dl $ver $lchecksums windows amd64
 }
 
-dl_ver ${1:-0.11.1}
+dl_ver ${1:-0.22.1}
